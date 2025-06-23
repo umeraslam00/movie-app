abc0a0d40cc41b19d683a090a6540d7b9d664a61
umeraslam00 <joreypull@gmail.com>
2025-06-23 09:35:57 +0500

    add onboarding screen and update color palette in Tailwind config

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,13 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 import './global.css';
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center font-extrabold">
-      <Text className="text-5xl text-blue-600">Welcome.</Text>
+      <Text className="text-5xl text-primary">Welcome.</Text>
+      <Link href="./onboarding">Onboarding</Link>
+      
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,22 @@ module.exports = {
   "./screens/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#151312",
+        accent: "#AB8BFF",
+        light: {
+          100: "#D6C6FF",
+          200: "#A8B5DB",
+          300: "#9CA4AB"
+        },
+        dark: {
+          100: "#221f3d",
+          200: "#0f0d23",
+        }
+      }
+    },
   },
   plugins: [],
 }
