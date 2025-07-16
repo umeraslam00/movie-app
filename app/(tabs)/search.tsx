feb1b5f0f9006f1368bd0b8407351021ac3e11d8
umeraslam00 <joreypull@gmail.com>
2025-07-16 09:43:43 +0500

    enhance MovieCard component with additional movie details and improve layout

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,41 @@
+import MovieCard from '@/components/MovieCard'
+import { images } from '@/constants/images'
+import { fetchMovies } from '@/services/api'
+import useFetch from '@/services/useFetch'
+import { useRouter } from 'expo-router'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Dimensions, FlatList, Image, View } from 'react-native'
 
 const search = () => {
+
+  const screenWidth = Dimensions.get('window').width;
+  const columnCount = screenWidth < 500 ? 2 : 3;
+
+  const router = useRouter()
+
+  const {
+    data: movies,
+    loading: loadingMovies,
+    error: errorMovies } = useFetch(() => fetchMovies({ query: "" })
+  )
+
   return (
-    <View>
-      <Text>search</Text>
+    <View className='flex-1 bg-primary'>
+
+      <Image source={images.bg} className='w-full z-0 flex-1 absolute' resizeMode='cover'/>
+
+      <FlatList
+        data={movies}
+        renderItem={({ item }) => MovieCard(item)}
+        keyExtractor={(item) => item.id.toString()}
+        className='px-5'
+        numColumns={columnCount}
+        columnWrapperStyle={{
+          justifyContent: 'space-between',
+          gap: 12
+        }}
+      />
+
     </View>
   )
 }
--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,6 +1,7 @@
+import { icons } from '@/constants/icons';
 import { Link } from 'expo-router';
 import React from 'react';
-import { Dimensions, Image, Text, TouchableOpacity } from 'react-native';
+import { Dimensions, Image, Text, TouchableOpacity, View } from 'react-native';
 
 const screenWidth = Dimensions.get('window').width;
 const columnCount = screenWidth < 500 ? 2 : 3;
@@ -9,7 +10,7 @@ const cardWidth = (screenWidth - spacing * (columnCount + 1)) / columnCount;
 
 const MovieCard = ({id, title, poster_path, vote_average, release_date}: Movie) => {
   return (
-    <Link href={`/movies/${id}`}>
+    <Link href={`/movies/${id}`} asChild>
       <TouchableOpacity style={{ width: cardWidth, marginBottom: spacing }}>
 
         <Image source={{
@@ -19,6 +20,15 @@ const MovieCard = ({id, title, poster_path, vote_average, release_date}: Movie) 
         }} style={{ width: '90%', aspectRatio: 2 / 3, borderRadius: 8 }} resizeMode='cover'/>
 
         <Text className='text-sm font-bold text-white mt-2'>{title}</Text>
+
+        <View className='flex flex-row items-center justify-start gap-x-2'>
+          <Image source={icons.star} className='size-4' />
+          <Text className='text-white font-bold'>{Math.round(vote_average)} /10</Text>
+        </View>
+
+        <View>
+          <Text className='text-xs font-bold text-light-300 mt-2'>{(release_date)?.split('-')[0]}</Text>
+        </View>
 
       </TouchableOpacity>
     </Link>
